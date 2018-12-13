d2adad75b0999360d2399d2849cdc10ef8d80210
John Mark Redding <johomurk@gmail.com>
2018-12-13 15:48:07 -0500

    Add Edit Page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,8 +4,22 @@ class CoursesController < ApplicationController
     erb :"courses/index"
   end
 
+  def find_course(id)
+    Course.find(id)
+  end
+
   get "/courses/:id" do
-    @course = Course.find(params[:id])
+    @course = find_course(params[:id])
     erb :"courses/show"
+  end
+  get "/courses/:id/edit" do
+    @course = find_course(params[:id])
+    erb :"courses/edit"
+  end
+
+  put "/courses/:id" do
+    @course = find_course(params[:id])
+    @course.update(params[:course])
+    redirect to "/courses/#{@course.id}"
   end
 end
